c12330ac3739c523145f8d31db40a0ef1d4a20b0
CAMERON ROBERTS <cameronldroberts@gmail.com>
2021-02-12 22:07:36 +0000

    improvements and additional retailers.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/cameronldroberts/btb
 
 go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.36.31 // indirect
+	github.com/go-rod/bypass v0.2.5 // indirect
+	github.com/go-rod/rod v0.89.1 // indirect
+)
--- main.go
+++ main.go
@@ -5,57 +5,178 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
 	"regexp"
 	"strings"
 	"time"
+
+	"github.com/go-rod/bypass"
+	"github.com/go-rod/rod"
+)
+
+var (
+	gamecount     int
+	aocount       int
+	argoscount    int
+	shoptocounter int
 )
 
 func main() {
 	fmt.Println("Beat the bot running....")
 	client := &http.Client{}
+
 	for {
-		currys(*client)
 		argos(*client)
 		ao(*client)
 		game(*client)
-		time.Sleep(10 * time.Second)
+		shopto(*client)
+		// smyths(*client)
+		// asda(*client)
+		// currys(*client)
+		// amazon(*client)
+
+		time.Sleep(5 * time.Second)
 	}
 
 }
-func currys(client http.Client) {
-	url := "https://www.currys.co.uk/gbuk/playstation-5-sony-1714-commercial.html"
-	req, _ := http.NewRequest("GET", url, nil)
-	res, err := client.Do(req)
-	if res.StatusCode >= 200 && res.StatusCode <= 299 {
-		bodyBytes, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal(err)
+func smyths(client http.Client) error {
+	// time.Sleep(time.Duration(rand.Intn(15)) * time.Second)
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+	page := bypass.MustPage(browser)
+	page.Timeout(time.Minute).MustNavigate("https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259")
+
+	element, err := page.Timeout(10 * time.Second).Element("body > div:nth-child(10) > section > div > div > div:nth-child(2) > div.detail_right.margn_tp_n > div.yCmsContentSlot.page-details-variants-select.js-addtocart-channal > div > div > div > div.AddToCart-PickUpInStoreAction > div.row > div.col-xs-6.js-changeDeliveryChannel.radioControl.option-selected > p")
+	if err != nil {
+		fmt.Println("smyths ", err)
+		page.MustScreenshot("")
+		return err
+	}
+
+	elementText, err := element.Text()
+	regx := regexp.MustCompile("Out Of Stock")
+	matches := regx.FindAllStringIndex(elementText, -1)
+
+	if len(matches) != 1 {
+		twilio(client, "SMYTHS", "https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259")
+		fmt.Println(time.Now().Clock())
+		fmt.Println("https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259 #######################################################################################")
+		time.Sleep(1 * time.Minute)
+
+	}
+	return nil
+}
+func asda(client http.Client) error {
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+	page := bypass.MustPage(browser)
+	page.MustNavigate("https://direct.asda.com/george/toys-character/gaming/gaming-consoles/playstation5-console/050887006,default,pd.html")
+	element, err := page.Timeout(10 * time.Second).Element("#main-header")
+	if err != nil {
+		fmt.Println("asda", err)
+		page.MustScreenshot("")
+		return err
+	}
+	elementText, err := element.Text()
+	if elementText != "In a spin?" {
+		fmt.Println("in stock ")
+		twilio(client, "ASDA", "https://direct.asda.com/george/toys-character/gaming/gaming-consoles/playstation5-console/050887006,default,pd.html")
+		fmt.Println(time.Now().Clock())
+		fmt.Println("https://direct.asda.com/george/toys-character/gaming/gaming-consoles/playstation5-console/050887006,default,pd.html ######################################################################################################")
+
+	}
+	return nil
+}
+
+func shopto(client http.Client) error {
+
+	time.Sleep(time.Duration(rand.Intn(15)) * time.Second)
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+	page := bypass.MustPage(browser)
+	page.Timeout(time.Minute).MustNavigate("https://www.shopto.net/en/search/?input_search=PlayStation%205%20Console&utm_source=Website&utm_campaign=PS5%20Console%20Xl")
+
+	element, err := page.Timeout(10 * time.Second).Element("#shop_content > div")
+	if err != nil {
+		fmt.Println("shopto ", err)
+		page.MustScreenshot("")
+		return err
+	}
+
+	elementText, err := element.Text()
+	regx := regexp.MustCompile("Sold out")
+	matches := regx.FindAllStringIndex(elementText, -1)
+	if len(matches) != 4 {
+		if shoptocounter == 0 {
+			twilio(client, "SHOPTO", "https://www.shopto.net/en/search/?input_search=PlayStation%205%20Console&utm_source=Website&utm_campaign=PS5%20Console%20Xl")
+			fmt.Println(time.Now().Clock())
+			fmt.Println("https://www.shopto.net/en/search/?input_search=PlayStation%205%20Console&utm_source=Website&utm_campaign=PS5%20Console%20Xl #######################################################################################")
+			time.Sleep(1 * time.Minute)
+			shoptocounter++
 		}
 
-		bodyString := string(bodyBytes)
-		regx := regexp.MustCompile("Playstation 5 sold out")
-		matches := regx.FindAllStringIndex(bodyString, -1)
-		if len(matches) != 1 {
-			twilio(client, "CURRYS", "https://www.currys.co.uk/gbuk/playstation-5-sony-1714-commercial.html")
-			for {
-				fmt.Println("https://www.currys.co.uk/gbuk/playstation-5-sony-1714-commercial.html ######################################################################################################")
-				time.Sleep(50 * time.Millisecond)
-			}
-		}
+	}
+	return nil
+}
+func amazon(client http.Client) error {
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+	page := bypass.MustPage(browser)
+	page.MustNavigate("https://www.amazon.co.uk/dp/B08H95Y452/ref=twister_B08J4RCVXW?_encoding=UTF8&psc=1")
+	element, err := page.Timeout(10 * time.Second).Element("#availability")
+	if err != nil {
+		fmt.Println("check screenshots ", err)
+		page.MustScreenshot("")
+		return err
+	}
+	availability, err := element.Text()
+	if strings.Contains(availability, "In stock.") {
+		fmt.Println("in stock ")
+		twilio(client, "AMAZON", "https://www.amazon.co.uk/dp/B08H95Y452/ref=twister_B08J4RCVXW?_encoding=UTF8&psc=1")
+		fmt.Println(time.Now().Clock())
+		fmt.Println("https://www.amazon.co.uk/dp/B08H95Y452/ref=twister_B08J4RCVXW?_encoding=UTF8&psc=1 ######################################################################################################")
+	}
+	return nil
+}
+
+func currys(client http.Client) error {
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+	page := bypass.MustPage(browser)
+	page.MustNavigate("https://www.currys.co.uk/gbuk/playstation-5-sony-1714-commercial.html")
+	page.MustElement("#onetrust-accept-btn-handler").MustClick()
+
+	element, err := page.Timeout(10 * time.Second).Element("#dsgContent > div.sold-out-banner")
+	if err != nil {
+		fmt.Println("check screenshots ", err)
+		page.MustScreenshot("")
+		return err
+	}
+	if element == nil {
+		fmt.Println("in stock ")
+		twilio(client, "CURRYS", "https://www.currys.co.uk/gbuk/playstation-5-sony-1714-commercial.html")
+		fmt.Println(time.Now().Clock())
+		fmt.Println("https://www.currys.co.uk/gbuk/playstation-5-sony-1714-commercial.html ######################################################################################################")
+
 	} else {
 		fmt.Println("CURRYS")
 		fmt.Println("error with request\n", err)
 	}
-
+	return nil
 }
 
-func ao(client http.Client) {
+func ao(client http.Client) error {
 	url := "https://ao.com/brands/playstation?cmredirectionvalue=ps5"
 	req, _ := http.NewRequest("GET", url, nil)
 	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		bodyBytes, err := ioutil.ReadAll(res.Body)
 		if err != nil {
@@ -66,25 +187,30 @@ func ao(client http.Client) {
 		regx := regexp.MustCompile("Sorry, PlayStation 5 is currently unavailable")
 		matches := regx.FindAllStringIndex(bodyString, -1)
 		if len(matches) != 1 {
-			twilio(client, "AO.COM", "https://ao.com/brands/playstation?cmredirectionvalue=ps5")
-			for {
+			if aocount == 0 {
+				twilio(client, "AO.COM", "https://ao.com/brands/playstation?cmredirectionvalue=ps5")
+				fmt.Println(time.Now().Clock())
 				fmt.Println("https://ao.com/brands/playstation?cmredirectionvalue=ps5 ######################################################################################################")
-				time.Sleep(50 * time.Millisecond)
+				aocount++
 			}
 		}
 	} else {
 		fmt.Println("ao.com")
 		fmt.Println("error with request\n", err)
 	}
-
+	return nil
 }
 
-func argos(client http.Client) {
+func argos(client http.Client) error {
 	url := "https://www.argos.co.uk/product/8349000/"
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("sec-ch-ua", "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
 	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 	if res.StatusCode >= 200 && res.StatusCode <= 299 {
 		bodyBytes, err := ioutil.ReadAll(res.Body)
 		if err != nil {
@@ -94,23 +220,29 @@ func argos(client http.Client) {
 		regx := regexp.MustCompile("Sorry, PlayStation®5 is currently unavailable.")
 		matches := regx.FindAllStringIndex(bodyString, -1)
 		if len(matches) != 1 {
-			twilio(client, "ARGOS", "https://www.argos.co.uk/product/8349000")
-			for {
+			if argoscount == 0 {
+				twilio(client, "ARGOS", "https://www.argos.co.uk/product/8349000")
+				fmt.Println(time.Now().Clock())
 				fmt.Println("https://www.argos.co.uk/product/8349000 ######################################################################################################")
-				time.Sleep(50 * time.Millisecond)
+				argoscount++
 			}
 		}
 	} else {
 		fmt.Println("ARGOS")
 		fmt.Println("error with request\n", err)
-	}
 
+	}
+	return nil
 }
 
-func game(client http.Client) {
+func game(client http.Client) error {
 	url := "https://assets.game.net/_master/hardwarePages/playstation5/merch/LaunchCopyAvail/ps5.js?_=1610833181756"
 	req, _ := http.NewRequest("GET", url, nil)
 	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 	if res.StatusCode >= 200 && res.StatusCode < 300 {
 		bodyBytes, err := ioutil.ReadAll(res.Body)
 		if err != nil {
@@ -121,17 +253,20 @@ func game(client http.Client) {
 		regx := regexp.MustCompile("Out of stock")
 		matches := regx.FindAllStringIndex(bodyString, -1)
 		if len(matches) != 2 {
-			twilio(client, "GAME", "https://www.game.co.uk/playstation-5")
-			for {
+
+			if gamecount == 0 {
+				twilio(client, "GAME", "https://www.game.co.uk/playstation-5")
+				fmt.Println(time.Now().Clock())
 				fmt.Println("https://www.game.co.uk/playstation-5 ######################################################################################################")
-				time.Sleep(50 * time.Millisecond)
+				fmt.Println("https://www.game.co.uk/en/hardware/playstation-5/?sortBy=PRICE_DESC&cm_sp=PlayStationHardwarePage-_-Hardware-_-825gbconsole ######################################################################################################")
+				gamecount++
 			}
 		}
 	} else {
 		fmt.Println("game")
 		fmt.Println("error with request\n", err)
 	}
-
+	return nil
 }
 
 func twilio(client http.Client, provider string, providerURL string) {
@@ -152,7 +287,10 @@ func twilio(client http.Client, provider string, providerURL string) {
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	resp, _ := client.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("twilio", err)
+	}
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		var data map[string]interface{}
 		decoder := json.NewDecoder(resp.Body)
@@ -161,6 +299,6 @@ func twilio(client http.Client, provider string, providerURL string) {
 			fmt.Println(data["sid"])
 		}
 	} else {
-		fmt.Println(resp.Status)
+		fmt.Println("twilio", resp.Status)
 	}
 }
